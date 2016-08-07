99366ea29361116c58951cace8a7fdfc060f7192
“gelidotris” <gelidotris@ecs.vuw.ac.nz>
2016-08-08 10:45:20 +1200

    Created basic item view page

--- routes/index.js
+++ routes/index.js
@@ -53,6 +53,11 @@ router.post('/addItem', function (req,res){
   console.log(req.body);
 });
 
+/* GET view item page. */
+router.get('/viewItem', function (req, res) {
+  res.render('viewItem', { title: 'Item View' });
+});
+
 /* GET login page. */
 router.get('/login', function(req, res, next) {
     res.render('login', { title: 'Log In' });
