9691fcf70cd0912008dd29a283cfe58173b502e9
borthwstan <stanton.borthwick2gmail.com>
2016-08-15 16:06:25 +1200

    Statscontroller should now return x pos for january - still needs testing though - could run into problems with ym WHERE statment in the SQL query

--- routes/StatsController.js
+++ routes/StatsController.js
@@ -11,22 +11,23 @@
 
 
     function getXPosJan() {
-        Date startJan = new Date().setFullYear(new Date().getFullYear(),0,0);
-        Date startFeb = new Date().setFullYear(new Date().getFullYear(),1,0);
-        Date startMarch = new Date().setFullYear(new Date().getFullYear(),2,0);
-        Date startApril = new Date().setFullYear(new Date().getFullYear(),3,0);
-        Date startMay = new Date().setFullYear(new Date().getFullYear(),4,0);
-        Date startJune = new Date().setFullYear(new Date().getFullYear(),5,0);
-        Date startJuly = new Date().setFullYear(new Date().getFullYear(),6,0);
-        Date startAug = new Date().setFullYear(new Date().getFullYear(),7,0);
-        Date startSept = new Date().setFullYear(new Date().getFullYear(),8,0);
-        Date startOct = new Date().setFullYear(new Date().getFullYear(),9,0)
-        Date startNov = new Date().setFullYear(new Date().getFullYear(),10,0)
-        Date startDec = new Date().setFullYear(new Date().getFullYear(),11,0)
+        var queryResult;
+        var startJan = new Date().setFullYear(new Date().getFullYear(),0,0);
+        var startFeb = new Date().setFullYear(new Date().getFullYear(),1,0);
+        var startMarch = new Date().setFullYear(new Date().getFullYear(),2,0);
+        var startApril = new Date().setFullYear(new Date().getFullYear(),3,0);
+        var startMay = new Date().setFullYear(new Date().getFullYear(),4,0);
+        var startJune = new Date().setFullYear(new Date().getFullYear(),5,0);
+        var startJuly = new Date().setFullYear(new Date().getFullYear(),6,0);
+        var startAug = new Date().setFullYear(new Date().getFullYear(),7,0);
+        var startSept = new Date().setFullYear(new Date().getFullYear(),8,0);
+        var startOct = new Date().setFullYear(new Date().getFullYear(),9,0);
+        var startNov = new Date().setFullYear(new Date().getFullYear(),10,0);
+        var startDec = new Date().setFullYear(new Date().getFullYear(),11,0);
 
         var queryJan = 'SELECT DateCollected, COUNT(DateCollected) AS collectedCount'
         queryJan += ' FROM items'
-        queryJan += ' WHERE DateCollected >= '
+        queryJan += ' WHERE (DateCollected >= :startJan) && (DateCollected < :startFeb)'
 
         //connect to db
         pg.connect(db, function (err, client, done) {
@@ -38,7 +39,7 @@
             }
             console.log("connection successful");
             //execute the search
-            client.query(queryJan, function (error, result) {
+            queryResult = client.query(queryJan, function (error, result) {
                 done();
                 if (error) {
                     console.log("query failed");
@@ -47,6 +48,7 @@
                 }
             });
         });
+        queryResult = queryResult*15;
     }
 
 }
