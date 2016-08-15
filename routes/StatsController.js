c1b9db469cfbf41a18fadf46f6d29c0e68482c1c
borthwstan <stanton.borthwick2gmail.com>
2016-08-15 15:05:31 +1200

    created date vars to split the graph up into 12

--- routes/StatsController.js
+++ routes/StatsController.js
@@ -8,13 +8,25 @@
     //location of our heroku DB
     var db = "postgres://kwumrsivhgpwme:OkWx2rA84KLrjTPOmSkOc2CIna@ec2-23-21-234-201.compute-1.amazonaws.com:5432/d54qeacf1ad3fc";
 
-    Date curYear = new Date().getFullYear();
-    Date curMonth = new Date().getMonth();
+
 
     function getXPosJan() {
+        Date startJan = new Date().setFullYear(new Date().getFullYear(),0,0);
+        Date startFeb = new Date().setFullYear(new Date().getFullYear(),1,0);
+        Date startMarch = new Date().setFullYear(new Date().getFullYear(),2,0);
+        Date startApril = new Date().setFullYear(new Date().getFullYear(),3,0);
+        Date startMay = new Date().setFullYear(new Date().getFullYear(),4,0);
+        Date startJune = new Date().setFullYear(new Date().getFullYear(),5,0);
+        Date startJuly = new Date().setFullYear(new Date().getFullYear(),6,0);
+        Date startAug = new Date().setFullYear(new Date().getFullYear(),7,0);
+        Date startSept = new Date().setFullYear(new Date().getFullYear(),8,0);
+        Date startOct = new Date().setFullYear(new Date().getFullYear(),9,0)
+        Date startNov = new Date().setFullYear(new Date().getFullYear(),10,0)
+        Date startDec = new Date().setFullYear(new Date().getFullYear(),11,0)
+
         var queryJan = 'SELECT DateCollected, COUNT(DateCollected) AS collectedCount'
-        queryJan += 'FROM items'
-        queryJan += 'WHERE DateCollected >= '
+        queryJan += ' FROM items'
+        queryJan += ' WHERE DateCollected >= '
 
         //connect to db
         pg.connect(db, function (err, client, done) {
