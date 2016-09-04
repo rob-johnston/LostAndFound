7beceefb5ba16bd0708d8ef0553c7c8d61ce4063
pangaaro <pangaaro@ecs.vuw.ac.nz>
2016-09-05 11:59:07 +1200

    Added query to login.js

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,9 @@
  */
 var express = require('express');
 var router = express.Router();
+var database = "postgres://kwumrsivhgpwme:OkWx2rA84KLrjTPOmSkOc2CIna@ec2-23-21-234-201.compute-1.amazonaws.com:5432/d54qeacf1ad3fc";
+var pg = require('pg').native;
+var username = null;
 
 /*GET login page*/
 router.get('/',function(req,res,next){
@@ -14,4 +17,9 @@ router.post('/', function(req,res,next){
     var USERNAME = req.body.user;
     var PASSWORD = req.body.password;
     username = req.body.user;
+    console.log(USERNAME + " " + PASSWORD);
+    var query = "SELECT * FROM Users WHERE username='%NAME%' AND password='%PASSWORD%';".replace("%NAME%", USERNAME).replace("%PASSWORD%", PASSWORD);
+    pg.connect(database, function(err,client,done){
+        
+    })
 });
