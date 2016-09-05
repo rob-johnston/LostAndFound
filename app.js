a13ed07050a3597fa1d14d15685663f79585e5eb
johnstrobe <johnstrobe@ecs.vuw.ac.nz>
2016-09-05 13:40:36 +1200

    implemented a login system using passport

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
